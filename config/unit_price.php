645dfde94d0071a14cf4ea9493dfa9b30bc1c90f
nasu togo <fuyuyasai105@gmail.com>
2022-10-03 18:40:04 +0000

    unit_price

--- config/unit_price.php
+++ config/unit_price.php
@@ -2,11 +2,11 @@
 
 return [ // 0.01g当たりの単価
   'unit_price' => [
-    'cbd_price' => '50',
-    'cbg_price' => '80',
-    'cbn_price' => '150',
-    'cbc_price' => '120',
-    'terpene_price' => '300',
+    'cbd_price' => '25',
+    'cbg_price' => '40',
+    'cbn_price' => '75',
+    'cbc_price' => '60',
+    'terpene_price' => '200',
   ]
 
 ];
